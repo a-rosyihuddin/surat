df51f94e73eaea18b88a106b78a8b9c24989c990
Ahmad Rosyihuddin <arosyihuddin6@gmail.com>
2022-06-22 00:25:38 +0700

    update codingan

--- desain/navbar.php
+++ desain/navbar.php
@@ -29,6 +29,7 @@
       <ul>
         <li><a href="../user/home.php">Home</a></li>
         <li><a href="../user/riwayat.php">Riwayat</a></li>
+        <li><a href="../user/jadwal.php">Jadwal Kerja</a></li>
         <li><a href="../proses_login/session_logout.php">Log-Out</a></li>
       </ul>
       <i class="bi bi-list mobile-nav-toggle"></i>
